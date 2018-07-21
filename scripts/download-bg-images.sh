8743316e3035800eb1350fce788a34d504e4b5a9
Christian Talmo <talmo.christian@gmail.com>
2018-07-21 16:30:20 +0300

    fix relative path

--- scripts/download-bg-images.sh
+++ scripts/download-bg-images.sh
@@ -5,7 +5,7 @@ URL_TEMPLATE="http://media.pldh.net/gallery/the493/{NAME}.jpg"
 mkdir tmp
 cd tmp && rm *
 
-for name in `cat ../pkmn-gen1.csv | sed "s/,/ /g"`
+for name in `cat "../../pkmn-gen1.csv" | sed "s/,/ /g"`
 do
   echo "$name"
   URL=`echo $URL_TEMPLATE | sed "s/{NAME}/$name/"`
